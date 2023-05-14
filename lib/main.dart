d8f4e5f6fab5dd99ddbdbfb5d01be24385c6e668
Marcin Gruchala <margruchala@gmail.com>
2023-05-14 18:24:35 +0200

    Setup firebase analytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'firebase_options.dart';
 import 'home_page.dart';
 
-void main() {
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  FirebaseAnalytics firebaseAnalytics = FirebaseAnalytics.instance;
+  firebaseAnalytics.setAnalyticsCollectionEnabled(true);
+
   runApp(const ProviderScope(child: MyApp()));
 }
 
