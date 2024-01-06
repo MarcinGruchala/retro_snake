78fad0641dac77dd3f03418b75b1e250aac79cc9
Marcin Gruchala <margruchala@gmail.com>
2024-01-06 14:16:12 +0100

    Add fake firebase options file

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'firebase_options.dart';
+import 'firebase/firebase_options.dart';
 import 'home_page.dart';
 import 'theme/theme_provider.dart';
 
